da724ee444e5cd8f3a31d991d4a317117a175c89
Mohammed Wajid <wajid17023@gmail.com>
2023-02-10 17:40:30 +0530

    added files

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import About from "./About";
+import Contact from "./Contact";
+import Home from './Home';
+import Services from './Services';
+
+
+const App = () => {
+  return (
+    <div>
+        <Home/>
+        <About/>
+        <Services/>
+        <Contact/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Index.css'
+
+const Index = () => {
+  return (
+    <div>Index</div>
+  )
+}
+
+export default Index
