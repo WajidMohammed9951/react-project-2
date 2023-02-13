d73fa562d374ebf52d275c572662c0e1cb44ab87
Mohammed Wajid <wajid17023@gmail.com>
2023-02-13 16:59:14 +0530

    added header and footer

--- src/App.js
+++ src/App.js
@@ -4,17 +4,21 @@ import Contact from './Contact'
 import Services from './Services'
 import Home from './Home'
 import {BrowserRouter, Routes, Route} from "react-router-dom"
+import Footer from './Components/Footer'
+import Header from './Components/Header'
 
 
 const App = () => {
   return (
    <BrowserRouter>
+   <Header/>
    <Routes>
     <Route path='/' element= {<Home/>}/>
     <Route path='/about' element= {<About/>}/>
     <Route path='/contact' element= {<Contact/>}/>
     <Route path='/services' element= {<Services/>}/>
    </Routes>
+   <Footer/>
    </BrowserRouter>
   )
 }
