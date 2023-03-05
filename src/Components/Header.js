25449029943bb3b36d90cf2af4b4118a8a909ac2
Mohammed Wajid <wajid17023@gmail.com>
2023-03-06 01:05:36 +0530

    styling

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,11 +1,24 @@
 import React from 'react';
 // import { NavLink } from 'react-router-dom';
 import NavBar from './NavBar';
+import styled from 'styled-components';
 
 const Header = () => {
   return (
+    <MainHeader>
    <NavBar/>  
+   </MainHeader>
   )
 }
+const MainHeader = styled.header`
+
+padding: 0 4.8rem;
+height: 10rem;
+background-color: ${({ theme }) => theme.colors.bg};
+display: flex;
+justify-content: flex-end;
+align-items: center;
+`
+
 
 export default Header
--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -7,7 +7,42 @@ export const GlobalStyle = createGlobalStyle
     padding:0;
     box-sizing:border-box;
     font-family: 'Work Sans', sans-serif;
-    font size: 62.5% rem;
-    overflow-x: hidden;     
+       
 }
+html{
+    font-size: 62.5% rem;
+    overflow-x: hidden; 
+}
+h1{
+    color: ${({theme}) => theme.colors.heading }
+    font-size: 6rem;
+    font-weight: 900;
+}
+
+h2 {
+    color: ${({ theme }) => theme.colors.heading};
+    font-size: 4.4rem;
+    font-weight: 300;
+    white-space: normal;
+    text-align: center;
+   }
+   h3 {
+   font-size: 1.8rem;
+   font-weight: 400;
+ }
+ p {
+   color: ${({ theme }) => theme.colors.text};
+   opacity: .7;
+   font-size: 1.65rem;
+   line-height: 1.5;
+   margin-top: 1rem;
+   font-weight:400;
+ }
+ a {
+    text-decoration: none;
+  }
+  li {
+    list-style: none;
+  }
+
 `;
