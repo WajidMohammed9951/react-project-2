d407aa4675da1f65ab1fa38ec65140740bb78ec9
Mohammed Wajid <wajid17023@gmail.com>
2023-03-10 02:44:42 +0530

    completed hero section

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -43,5 +43,24 @@ h2 {
   li {
     list-style: none;
   }
+  
+.container {
+  max-width: 120rem;
+  margin: 0 auto;
+  margin-left:1rem;
+}
+.grid {
+  display: grid;
+  gap: 9rem;
+}
+.grid-two-column {
+  grid-template-columns: repeat(2, 1fr);
+}
+.grid-three-column {
+  grid-template-columns: repeat(3, 1fr);
+}
+.grid-four-column{
+   grid-template-columns: 1fr 1.2fr .5fr .8fr ;
+}
 
 `;
