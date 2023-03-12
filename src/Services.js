2cafb5e7111ad5a56b355c607e3471e4b0171bc5
Mohammed Wajid <wajid17023@gmail.com>
2023-03-12 20:19:34 +0530

    styling services section

--- src/Services.js
+++ src/Services.js
@@ -1,47 +1,65 @@
-import React from 'react'
+import React from "react";
+import styled from "styled-components";
 
-const Service = () => {
+const Services = () => {
   let service_details = [
     {
-    id: 'e1',
-    title:'Web development',
-    image:'./images/web deve.avif',
-    description:'Web development, best courses are available'
-  },
-  {
-    id: 'e2',
-    title:'Application development ',
-    image:'./images/application deve.avif',
-    description:'Application development,best courses are available'
-  },
-  {
-    id: 'e3',
-    title:'Software development',
-    image:'./images/software deve.avif',
-    description:'Software development,best courses are available '
-  },
-  {
-    id: 'e4',
-    title:'Digital marketing',
-    image:'./images/digital marketing.avif',
-    description:'Digital marketing,best courses are available  '
-  },
-  {
-    id: 'e3',
-    title:'Graphic designing',
-    image:'./images/graphic designing.avif',
-    description:'Graphic designing,best courses are available'
-  },
-  {
-    id: 'e3',
-    title:'Wajid Codefest',
-    image:'./images/wajid codefest.avif',
-    description:'Wajid Codefest, best courses are available'
-  }
-  ]
+      id: "e1",
+      title: "Web development",
+      image: "./images/web deve.avif",
+      description: "Web development, best courses are available",
+    },
+    {
+      id: "e2",
+      title: "Application development ",
+      image: "./images/application deve.avif",
+      description: "Application development,best courses are available",
+    },
+    {
+      id: "e3",
+      title: "Software development",
+      image: "./images/software deve.avif",
+      description: "Software development,best courses are available ",
+    },
+    {
+      id: "e4",
+      title: "Digital marketing",
+      image: "./images/digital marketing.avif",
+      description: "Digital marketing,best courses are available  ",
+    },
+    {
+      id: "e3",
+      title: "Graphic designing",
+      image: "./images/graphic designing.avif",
+      description: "Graphic designing,best courses are available",
+    },
+    {
+      id: "e3",
+      title: "Wajid Codefest",
+      image: "./images/wajid codefest.avif",
+      description: "Wajid Codefest, best courses are available",
+    },
+  ];
   return (
+    <Wrapper className="section">
+      <h2 className="common-heading">Our Services</h2>
+      <div className="container grid grid-three-column">
+         {
+         Services.map((curElem) => {
+           const { id, title, image, description } = curElem;
+           return <div key={id} className="card">
+            
+             </div>
 
-  )
-}
+           
 
-export default Services
+         })}
+         
+
+      </div>
+    </Wrapper>
+  );
+};
+const Wrapper = styled.section``;
+
+export default Services;
