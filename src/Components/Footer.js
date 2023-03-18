5c839790c027447cff00657f3434e42e0990c4c5
Mohammed Wajid <wajid17023@gmail.com>
2023-03-19 01:52:04 +0530

    footer making

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,9 +1,119 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
+import styled from 'styled-components'
+import { Button } from '../Styles/Button'
+import { FaDiscord, FaInstagram, FaYoutube} from "react-icons/fa";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <Wrapper>
+      <section className='contact-short'>
+        <div className='comp-div'>
+          <div >
+            <h3>Ready to get started</h3>
+            <h3>Talk to us today</h3>
+          </div>
+          <div>
+            <NavLink to="/">
+              <Button className='btn'> Get started</Button>
+            </NavLink>
+          </div>
+
+
+        </div>
+      </section>
+
+
+      <footer>
+        <div className="container grid grid-four-column">
+          <div className="footer-about">
+            <h3>Thapa Technical</h3>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+          </div>
+          </div>
+
+
+          {/* 2nd column */}
+          <div className="footer-subscribe">
+            <h3>Subscribe to get important updates</h3>
+            <form action="#">
+              <input
+                type="email"
+                required
+                autoComplete="off"
+                placeholder="Email"
+              />
+              <input type="submit" value="Subscribe" />
+            </form>
+          </div>
+
+          {/* 3rs column  */}
+          <div className="footer-social">
+            <h3>Follows Us</h3>
+            <div className="footer-social--icons">
+              <div>
+                <FaDiscord className="icons" />
+              </div>
+              <div>
+                <FaInstagram className="icons" />
+              </div>
+              <div>
+                <a
+                  href="https://www.youtube.com/channel/UCwfaAHy4zQUb2APNOGXUCCA"
+                  target="_blank">
+                  <FaYoutube className="icons" />
+                </a>
+              </div>
+            </div>
+          </div>
+
+      </footer>
+    </Wrapper>
+  );
+};
+const Wrapper = styled.section`
+.contact-short {
+  max-width: 60vw;
+  margin: auto;
+  padding: 3rem 5rem;
+  background-color: ${({ theme }) => theme.colors.bg};
+  border-radius: 1rem;
+  box-shadow: ${({ theme }) => theme.colors.shadowSupport};
+ 
+ 
+ .comp-div{
+  display:flex;
+
+ }
+ .btn{
+  margin-left:19rem;
+  margin-top:1.5rem;
+ }
+ footer {
+  padding: 14rem 0 9rem 0;
+  background-color: ${({ theme }) => theme.colors.footer_bg};
+  h3 {
+    color: ${({ theme }) => theme.colors.hr};
+    margin-bottom: 2.4rem;
+  }
+  p {
+    color: ${({ theme }) => theme.colors.white};
+  }
+  .footer-social--icons {
+    display: flex;
+    gap: 2rem;
+    div {
+      padding: 1rem;
+      border-radius: 50%;
+      border: 2px solid ${({ theme }) => theme.colors.white};
+      .icons {
+        color: ${({ theme }) => theme.colors.white};
+        font-size: 2.4rem;
+        position: relative;
+        cursor: pointer;
+      }
+    }
+  }
+`
 
 export default Footer
