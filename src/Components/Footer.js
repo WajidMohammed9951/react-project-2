74d79993cedddf63710d01ee303b7cc4cd0d3314
Mohammed Wajid <wajid17023@gmail.com>
2023-03-19 02:12:53 +0530

    footer completed

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -7,31 +7,29 @@ import { FaDiscord, FaInstagram, FaYoutube} from "react-icons/fa";
 const Footer = () => {
   return (
     <Wrapper>
-      <section className='contact-short'>
-        <div className='comp-div'>
-          <div >
-            <h3>Ready to get started</h3>
+      <section className="contact-short">
+        <div className="grid grid-two-column">
+          <div>
+            <h3>Ready to get started?</h3>
             <h3>Talk to us today</h3>
           </div>
-          <div>
+
+          <div className="contact-short-btn">
             <NavLink to="/">
-              <Button className='btn'> Get started</Button>
+              <Button>Get Started</Button>
             </NavLink>
           </div>
-
-
         </div>
       </section>
 
+      {/* footer section  */}
 
       <footer>
         <div className="container grid grid-four-column">
           <div className="footer-about">
-            <h3>Thapa Technical</h3>
+            <h3>Wajid Mohammed</h3>
             <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
           </div>
-          </div>
-
 
           {/* 2nd column */}
           <div className="footer-subscribe">
@@ -67,53 +65,95 @@ const Footer = () => {
             </div>
           </div>
 
+          {/* 4th column  */}
+          <div className="footer-contact">
+            <h3>Call Us</h3>
+            <h3>+91 12345678968</h3>
+          </div>
+        </div>
+
+        {/* bottom section  */}
+        <div className="footer-bottom--section">
+          <hr />
+          <div className="container grid grid-two-column">
+            <p>
+              @{new Date().getFullYear()} ThapaTechnical. All Rights Reserved
+            </p>
+            <div>
+              <p>PRIVACY POLICY</p>
+              <p>TERMS & CONDITIONS</p>
+            </div>
+          </div>
+        </div>
       </footer>
     </Wrapper>
   );
 };
-const Wrapper = styled.section`
-.contact-short {
-  max-width: 60vw;
-  margin: auto;
-  padding: 3rem 5rem;
-  background-color: ${({ theme }) => theme.colors.bg};
-  border-radius: 1rem;
-  box-shadow: ${({ theme }) => theme.colors.shadowSupport};
- 
- 
- .comp-div{
-  display:flex;
 
- }
- .btn{
-  margin-left:19rem;
-  margin-top:1.5rem;
- }
- footer {
-  padding: 14rem 0 9rem 0;
-  background-color: ${({ theme }) => theme.colors.footer_bg};
-  h3 {
-    color: ${({ theme }) => theme.colors.hr};
-    margin-bottom: 2.4rem;
+const Wrapper = styled.section`
+  .contact-short {
+    max-width: 60vw;
+    margin: auto;
+    padding: 5rem 10rem;
+    background-color: ${({ theme }) => theme.colors.bg};
+    border-radius: 1rem;
+    box-shadow: ${({ theme }) => theme.colors.shadowSupport};
+    transform: translateY(50%);
   }
-  p {
-    color: ${({ theme }) => theme.colors.white};
+  .contact-short-btn {
+    justify-self: end;
+    align-self: center;
   }
-  .footer-social--icons {
-    display: flex;
-    gap: 2rem;
-    div {
-      padding: 1rem;
-      border-radius: 50%;
-      border: 2px solid ${({ theme }) => theme.colors.white};
-      .icons {
-        color: ${({ theme }) => theme.colors.white};
-        font-size: 2.4rem;
-        position: relative;
-        cursor: pointer;
+  footer {
+    padding: 14rem 0 9rem 0;
+    background-color: ${({ theme }) => theme.colors.footer_bg};
+    h3 {
+      color: ${({ theme }) => theme.colors.hr};
+      margin-bottom: 2.4rem;
+    }
+    p {
+      color: ${({ theme }) => theme.colors.white};
+    }
+    .footer-social--icons {
+      display: flex;
+      gap: 2rem;
+      div {
+        padding: 1rem;
+        border-radius: 50%;
+        border: 2px solid ${({ theme }) => theme.colors.white};
+        .icons {
+          color: ${({ theme }) => theme.colors.white};
+          font-size: 2.4rem;
+          position: relative;
+          cursor: pointer;
+        }
+      }
+    }
+    .footer-bottom--section {
+      padding-top: 9rem;
+      hr {
+        margin-bottom: 2rem;
+        color: ${({ theme }) => theme.colors.hr};
+        height: 0.1px;
       }
     }
   }
-`
+  @media (max-width: ${({ theme }) => theme.media.mobile}) {
+    .contact-short {
+      max-width: 95vw;
+      padding: 2rem 0rem;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      .contact-short-btn {
+        text-align: center;
+        justify-self: flex-start;
+      }
+    }
+    footer .footer-bottom--section {
+      padding-top: 3.2rem;
+    }
+  }
+`;
 
 export default Footer
